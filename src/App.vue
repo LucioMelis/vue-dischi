06f9cfe20a828ce9e6e05a2ecf4ded8d3e9feb05
luciomelis <luciomelis1992@gmail.com>
2022-04-08 15:37:46 +0200

    Header terminato

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,30 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png" />
     <HeaderSpotify />
+    <MainSpotify />
   </div>
 </template>
 
 <script>
 import HeaderSpotify from "./components/HeaderSpotify.vue";
+import MainSpotify from "./components/MainSpotify.vue";
 
 export default {
   name: "App",
   components: {
     HeaderSpotify,
+    MainSpotify,
   },
 };
 </script>
 
 <style lang="scss">
+* {
+  padding: 0;
+  margin: 0;
+  box-sizing: border-box;
+}
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
 }
 </style>
--- src/components/HeaderSpotify.vue
+++ src/components/HeaderSpotify.vue
@@ -1,5 +1,9 @@
 <template>
-  <div class="hello"></div>
+  <div class="container-header">
+    <div>
+      <img src="../assets/spotify-logo-2.png" alt="logo-spotify" />
+    </div>
+  </div>
 </template>
 
 <script>
@@ -10,4 +14,15 @@ export default {
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped lang="scss">
+@import "src/variabili.scss";
+.container-header {
+  padding: 10px 15px;
+  background-color: $bg-secondary;
+  div {
+    width: 50px;
+    img {
+      width: 100%;
+    }
+  }
+}
 </style>
