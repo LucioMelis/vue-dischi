dc72aa7ea5aac20fab936a8376c198fd8bcf810a
luciomelis <luciomelis1992@gmail.com>
2022-04-11 20:56:24 +0200

    esercizio Terminato inserendo un nuovo componente utilizzando $emit

--- src/components/MainSpotify.vue
+++ src/components/MainSpotify.vue
@@ -1,24 +1,11 @@
 <template>
   <div class="container-main">
     <!-- Sezione Option -->
-    <div class="select-disc">
-      <h4>Seleziona il genere:</h4>
-      <select
-        name="disc-option"
-        v-model="genereSelezionato"
-        @change="cambioGenere()"
-      >
-        <option value="All">All</option>
-        <option value="Pop">Pop</option>
-        <option value="Metal">Metal</option>
-        <option value="Rock">Rock</option>
-        <option value="Jazz">Jazz</option>
-      </select>
-    </div>
+    <SelectSearch @ricerca="filtroGenere" />
     <!-- Sezione Dischi  -->
     <div v-if="disc.length > 0" class="container-card">
       <CardDisc
-        v-for="(song, index) in cambioGenere()"
+        v-for="(song, index) in cambioGenere"
         :key="index"
         :songdisc="song"
       />
@@ -34,17 +21,19 @@
 import CardDisc from "../components/CardDisc.vue";
 import axios from "axios";
 import Loading from "../components/Loading.vue";
+import SelectSearch from "../components/SelectSearch.vue";
 
 export default {
   name: "MainSpotify",
   components: {
     CardDisc,
     Loading,
+    SelectSearch,
   },
   data() {
     return {
       disc: [],
-      genereSelezionato: "All",
+      genere: "",
     };
   },
   mounted() {
@@ -57,17 +46,17 @@ export default {
       });
   },
   methods: {
+    filtroGenere(genereSelezionato) {
+      console.log(genereSelezionato);
+      this.genere = genereSelezionato;
+    },
+  },
+  computed: {
     cambioGenere() {
-      console.log(this.genereSelezionato);
-      if (this.genereSelezionato === "All") {
+      if (this.genere === "All") {
         return this.disc;
       }
-      return this.disc.filter((item) =>
-        item.genre.includes(this.genereSelezionato)
-      );
-      // inserire il filtro dell'array in base al genere selezinato
-      // una volta che funziona questo faccio un commit e procedo a fare
-      // il componente con l $emit
+      return this.disc.filter((item) => item.genre.includes(this.genere));
     },
   },
 };
@@ -79,23 +68,6 @@ export default {
 .container-main {
   min-height: calc(100vh - 74px);
   background-color: $bg-primary;
-  .select-disc {
-    display: flex;
-    justify-content: center;
-    align-items: center;
-    padding: 10px 0;
-    h4 {
-      color: $text-color;
-      padding-right: 10px;
-    }
-    select {
-      border: 1px solid $bg-secondary;
-      background-color: $bg-secondary;
-      color: white;
-      border-radius: 10px;
-      padding: 5px;
-    }
-  }
   .container-card {
     display: flex;
     flex-wrap: wrap;
---
+++ src/components/SelectSearch.vue
@@ -0,0 +1,49 @@
+<template>
+  <div class="select-disc">
+    <h4>Seleziona il genere:</h4>
+    <select
+      name="disc-option"
+      v-model="genereSelezionato"
+      @change="$emit('ricerca', genereSelezionato)"
+    >
+      <option value="All">All</option>
+      <option value="Pop">Pop</option>
+      <option value="Metal">Metal</option>
+      <option value="Rock">Rock</option>
+      <option value="Jazz">Jazz</option>
+    </select>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "SelectSearch",
+  data() {
+    return {
+      genereSelezionato: "All",
+    };
+  },
+};
+</script>
+
+<style lang="scss" scoped>
+@import "src/variabili.scss";
+.select-disc {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  padding: 10px 0;
+  h4 {
+    color: $text-color;
+    padding-right: 10px;
+  }
+  select {
+    border: 1px solid $bg-secondary;
+    background-color: $bg-secondary;
+    color: white;
+    border-radius: 10px;
+    padding: 5px;
+  }
+}
+</style>
+
